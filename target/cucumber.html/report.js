225ef23f310ce4f5138c6f2bc4ce5375822dd03d
dhivyagit2022 <divya.rs89@gmail.com>
2022-04-10 07:43:52 +0000

    phase2project

---
+++ target/cucumber.html/report.js
@@ -0,0 +1,329 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/features/Products.feature");
+formatter.feature({
+  "name": "This feature would be used to validate the Product prices",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "Open the Swag Labs application",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User is on SauceDemo login page",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User enters Username \"standard_user\" and Password \"secret_sauce\"",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User clicks on Login button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "To validate the Product Prices",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User is on Products Page",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User compare the following products price",
+  "rows": [
+    {
+      "cells": [
+        "Sauce Labs Backpack",
+        "$29.99"
+      ]
+    },
+    {
+      "cells": [
+        "Sauce Labs Bike Light",
+        "$9.99"
+      ]
+    }
+  ],
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.uri("file:src/test/java/features/login.feature");
+formatter.feature({
+  "name": "This feature would be used to design the Login page of the application",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "To validate the positive login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User enters Username \"\u003cusername\u003e\" and Password \"\u003cpassword\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "User clicks on Login button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User lands on the Product Page",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "standard_user",
+        "secret_sauce"
+      ]
+    },
+    {
+      "cells": [
+        "problem_user",
+        "secret_sauce"
+      ]
+    }
+  ]
+});
+formatter.background({
+  "name": "Open the Swag Labs application",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User is on SauceDemo login page",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "To validate the positive login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User enters Username \"standard_user\" and Password \"secret_sauce\"",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User clicks on Login button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User lands on the Product Page",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "Open the Swag Labs application",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User is on SauceDemo login page",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "To validate the positive login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User enters Username \"problem_user\" and Password \"secret_sauce\"",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User clicks on Login button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User lands on the Product Page",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenarioOutline({
+  "name": "To validate the negative login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User enters Username \"\u003cusername\u003e\" and Password \"\u003cpassword\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "User clicks on Login button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User gets the error message as \"Epic sadface: Username and password do not match any user in this service\"",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "apple",
+        "secret_sauce"
+      ]
+    },
+    {
+      "cells": [
+        "random_user",
+        "secret_sauce"
+      ]
+    }
+  ]
+});
+formatter.background({
+  "name": "Open the Swag Labs application",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User is on SauceDemo login page",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "To validate the negative login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User enters Username \"apple\" and Password \"secret_sauce\"",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User clicks on Login button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User gets the error message as \"Epic sadface: Username and password do not match any user in this service\"",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "Open the Swag Labs application",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User is on SauceDemo login page",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "To validate the negative login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User enters Username \"random_user\" and Password \"secret_sauce\"",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User clicks on Login button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "User gets the error message as \"Epic sadface: Username and password do not match any user in this service\"",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
